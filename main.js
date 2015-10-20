f718a2b260b1a3b286fa81ae2833a5446c414847
tken85 <terrykennair@Terrys-MBP.attlocal.net>
2015-10-19 21:26:09 -0400

    jquery nightmare mode

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 
-
 // define standard read speed
 var readSpeed = 230;
 
 // allow user to change read speed. Values from http://www.forbes.com/sites/brettnelson/2012/06/04/do-you-read-fast-enough-to-be-successful/
 
 // grab text
-var allText= document.getElementById('post').textContent;
+
+//var allText= document.getElementById('post').textContent;- this is the javascript version used before converting to jquery
+
+var allText = $('#post').text();
 
 //make text into array
 var textArr = allText.split(" ");
-// remove mistakes from array
 
+// remove mistakes from array in the form of punctation or spacing
 function notSpace(value){
-  return value !="";
+  return (value !="" && value !="\n" && value !="-" && value !="--" && value !="." && value !="," && value !="?" && value !=";" && value !="!");
 }
+
 var filteredArr = textArr.filter(notSpace);
 
 var timeToRead = function(array){
@@ -26,9 +29,10 @@ var timeToRead = function(array){
   return result;
 }
 
-var readTime = Math.round(timeToRead(textArr));
+var readTime = Math.round(timeToRead(filteredArr));
 
-document.getElementById('time').innerHTML = readTime +" minutes";
+ $('#time').html(readTime + " minutes to read");
+/*document.getElementById('time').innerHTML = readTime +" minutes to read";*/
 
 // allow user to change read speed. Values from http://www.forbes.com/sites/brettnelson/2012/06/04/do-you-read-fast-enough-to-be-successful/
 
@@ -38,57 +42,43 @@ var setReadSpeed = function(selection){
   case 0:
     readSpeed = 150;
     readTime = Math.round(timeToRead(textArr));
-    document.getElementById('time').innerHTML = readTime +" minutes";
+    $('#time').html(readTime + " minutes to read");
+    //document.getElementById('time').innerHTML = readTime +" minutes to read"; - javascript version
     break;
 
   case 1:
     readSpeed = 230;
     readTime = Math.round(timeToRead(textArr));
-    document.getElementById('time').innerHTML = readTime +" minutes";
+    $('#time').html(readTime + " minutes to read");
+    //document.getElementById('time').innerHTML = readTime +" minutes to read"; - javascript version
     break;
 
   case 2:
     readSpeed = 575;
     readTime = Math.round(timeToRead(textArr));
-    document.getElementById('time').innerHTML = readTime +" minutes";
+    $('#time').html(readTime + " minutes to read");
+    //document.getElementById('time').innerHTML = readTime +" minutes to read"; - javascript version
     break;
 
   case 3:
     readSpeed = 1500;
     readTime = Math.round(timeToRead(textArr));
-    document.getElementById('time').innerHTML = readTime +" minutes";
+    $('#time').html(readTime + " minutes to read");
+    //document.getElementById('time').innerHTML = readTime +" minutes to read"; - javascript version
   }
 }
 
-var setHitch = function(){
-  document.getElementById('post').innerHTML = hitch;
-  document.getElementById('title').innerHTML = "Hitchhiker's Guide";
-  allText= document.getElementById('post').textContent;
+// allow user to switch post and recalculate
+var setPost = function(whichPost){
+  $('#post').html(whichPost.text);
+  $('#title').html(whichPost.title);
+  //document.getElementById('post').innerHTML = whichPost.text; - javascript version
+  //document.getElementById('title').innerHTML = whichPost.title; - javascript version
+  allText = $('#post').text();
+  //allText= document.getElementById('post').textContent; - javascript version
   textArr = allText.split(" ");
   filteredArr = textArr.filter(notSpace);
   readTime = Math.round(timeToRead(textArr));
-  document.getElementById('time').innerHTML = readTime +" minutes";
-
-}
-
-var setCook = function(){
-  document.getElementById('post').innerHTML = cookBook;
-  document.getElementById('title').innerHTML = "Random Cookbook";
-  allText= document.getElementById('post').textContent;
-  textArr = allText.split(" ");
-  filteredArr = textArr.filter(notSpace);
-  readTime = Math.round(timeToRead(textArr));
-  document.getElementById('time').innerHTML = readTime +" minutes";
-
-}
-
-var setGetty = function(){
-  document.getElementById('post').innerHTML = gettysburg;
-  document.getElementById('title').innerHTML = "Gettysburg Address";
-  allText= document.getElementById('post').textContent;
-  textArr = allText.split(" ");
-  filteredArr = textArr.filter(notSpace);
-  readTime = Math.round(timeToRead(textArr));
-  document.getElementById('time').innerHTML = readTime +" minutes";
-
+  $('#time').html(readTime + " minutes to read");
+  //document.getElementById('time').innerHTML = readTime +" minutes to read"; - javascript version
 }
